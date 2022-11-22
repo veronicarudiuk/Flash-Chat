41e3876d3fddefc70d417d25b3eb32bad3ee632d
Veronica Rudiuk <veronicarudiuk@gmail.com>
2022-11-22 18:31:11 +0400

    add Firebase pods and add registration functionality with error alert

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,27 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    let alertController = UIAlertController(title: "Error", message: "\(e.localizedDescription)", preferredStyle: .alert)
+                    let action = UIAlertAction(title: "Ok", style: .default) { (action) in }
+                    alertController.addAction(action)
+                    self.present(alertController, animated: true)
+                } else {
+                    //                    Navigate to the chat VC
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
     }
     
 }
